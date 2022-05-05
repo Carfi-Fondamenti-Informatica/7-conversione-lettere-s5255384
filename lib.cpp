399294c2044e4d664f96f07f5ed8b8aa02b914c2
s5255384 <100850996+s5255384@users.noreply.github.com>
2022-05-05 17:33:01 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include"lib.h"
 
+using namespace std;
+
+bool Carattere (char &x){
+
+    if(x>='a' && x<='z'){
+        x=x+224;
+        return true;
+    }
+    else if(x>='A' && x<='Z'){
+        x=x+32;
+        return true;
+    }
+
+    else
+        return false;
+
+}
